186744ef200638558bf37660c746001c840be28e
deekCE <49599456+deekCE@users.noreply.github.com>
2022-03-24 19:39:23 +0000

    Changed border size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Meslo LG M:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 7;
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
